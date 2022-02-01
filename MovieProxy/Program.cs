fca7b8f0c4e20490ca8c9791d126f5646a6a9fb3
Jeremiah Chienda <jeremiahchienda@gmail.com>
2022-02-01 20:37:42 +0200

    added a landing endpoint

--- MovieProxy/Program.cs
+++ MovieProxy/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IMovieService, ImdbMovieService>();
 builder.Services.AddResponseCaching();
+var apiInfo = new ApiAInfo("MovieProxy", "v1", "https://docs.microsoft.com/en-us/aspnet/core/tutorials/web-api-help-pages");
+
 
 var app = builder.Build();
 app.UseSwagger();
@@ -17,6 +19,10 @@ app.MapGet("/api/recent-movies",
     async (IMovieService movieService) => await movieService.GetRecentlyAddedMovies());
 app.MapGet("/api/random-movies",
     async (IMovieService movieService) => await movieService.GetRandomMovies());
-app.MapGet("/", () => "Movie Proxy API");
+
+app.MapGet("/", () => "Welcome to MovieProxy")
+    .ExcludeFromDescription();
 
 app.Run();
+
+internal record ApiAInfo(string Name, string Version, string Documentation);
